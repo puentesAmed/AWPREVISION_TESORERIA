5f506ea5493ba697830e9bf21d7271c83f539768
puentesAmed <puentesamed1@gmail.com>
2025-10-02 18:27:27 +0200

    Pequeños ajustes

--- backend/src/routes/cashflows.routes.js
+++ backend/src/routes/cashflows.routes.js
@@ -1,16 +1,3 @@
-/*import { Router } from 'express'
-import { roles } from '../middleware/auth.js'
-import { list, create, update, remove, postNow } from '../controllers/cashflows.controller.js'
-const r = Router()
-
-r.get('/', list)
-r.post('/', roles('admin','fin'), create)
-r.patch('/:id', roles('admin','fin'), update)
-r.delete('/:id', roles('admin'), remove)
-r.post('/:id/post', roles('admin','fin'), postNow)
-export default r
-*/
-
 import { Router } from 'express'
 import { list,createCashflow,updateCashflow,removeCashflow,calendar, upcoming, importCashflows } from '../controllers/cashflows.controller.js'
 import multer from 'multer';
